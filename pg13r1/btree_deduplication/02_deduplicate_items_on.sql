413a7d719439e15ccef3932f6e3f4ddd08396f87
sql2 <hyeongchae@gmail.com>
2020-11-13 15:46:17 +0900

    add timing on

--- pg13r1/btree_deduplication/02_deduplicate_items_on.sql
+++ pg13r1/btree_deduplication/02_deduplicate_items_on.sql
@@ -5,6 +5,7 @@ CREATE TABLE btree_text (col1 text collate "C");
 CREATE INDEX dedup_idx ON btree_text(col1)
 WITH (deduplicate_items=on);
 
+\timing on
 INSERT INTO btree_text SELECT 'foo' FROM generate_series(1,100000);
 
 SELECT pg_size_pretty (pg_relation_size('dedup_idx'));  
